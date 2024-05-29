eca21e3958ded79c235051f9dbec2ec4a884a3a2
porkieeeeeee <porkieeeeeee@gmail.com>
2024-05-29 15:00:26 +0900

    [INIT] 환경세팅

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,12 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {
-            colors: {
-                primary: "#4c6ef5",
-                gray000: "#fff",
-                gray100: "#495057",
-                gray200: "#292e33",
-                gray300: "#212529",
-            },
-            backgroundColor: (theme) => ({
-                ...theme("colors"),
-            }),
-            textColor: (theme) => ({
-                ...theme("colors"),
-            }),
-            borderColor: (theme) => ({
-                ...theme("colors"),
-            }),
+        colors: {
+            primary: "#4c6ef5",
+            gray000: "#fff",
+            gray100: "#495057",
+            gray200: "#292e33",
+            gray300: "#212529",
         },
     },
     plugins: [],
---
+++ twin.d.ts
@@ -0,0 +1,7 @@
+import "twin.macro";
+import styledImport, { css as cssImport } from "styled-components";
+
+declare module "twin.macro" {
+    const styled: typeof styledImport;
+    const css: typeof cssImport;
+}
