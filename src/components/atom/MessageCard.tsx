88798e0b1c1087b810cc338803d6f6ab2f576e18
porkieeeeeee <porkieeeeeee@gmail.com>
2024-05-29 17:37:20 +0900

    feat::MessageCard 개발

---
+++ src/components/atom/MessageCard.tsx
@@ -0,0 +1,35 @@
+import { messageIcon } from "assets/imagesManagement";
+import tw, { css, styled } from "twin.macro";
+
+interface IMessageCardProps {
+    text: string;
+    hasScroll: boolean;
+}
+
+const MessageCard = ({ text, hasScroll }: IMessageCardProps) => {
+    return (
+        <Container hasScroll={hasScroll}>
+            <Message>{text}</Message>
+        </Container>
+    );
+};
+
+const Container = styled.div<{ hasScroll: boolean }>`
+    ${tw`py-[16px] pl-[84px] pr-[14px] bg-gray000 rounded`};
+
+    ${({ hasScroll }) =>
+        hasScroll &&
+        css`
+            margin-right: 4px;
+        `}
+`;
+
+const Message = styled.p`
+    ${tw`relative text-gray300 text-xs font-normal leading-4`};
+    ::before {
+        ${tw`content-[''] absolute top-[6%] left-[-70px] translate-y-[-50%] w-[50px] h-[50px]`};
+        background: url(${messageIcon}) center / contain no-repeat;
+    }
+`;
+
+export default MessageCard;
