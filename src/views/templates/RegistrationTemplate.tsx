fe462a2d9c3918fa21a188b3960adcc238cf5399
porkieeeeeee <porkieeeeeee@gmail.com>
2024-05-30 15:50:36 +0900

    feat::입력값 로컬 스토리지 저장

--- src/views/templates/RegistrationTemplate.tsx
+++ src/views/templates/RegistrationTemplate.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import HyperlinkButton from "components/atom/HyperlinkButton";
 import MainTitle from "components/atom/MainTitle";
 import TextArea from "components/atom/TextArea";
-import { showToast, EToastType } from "components/atom/Toast"; // showToast와 EToastType을 가져옵니다.
+import { showToast, EToastType } from "components/atom/Toast";
 import SingleCenterItemBox from "components/molecule/SingleCenterItemBox";
 
 const RegistrationTemplate = () => {
     const [text, setText] = useState<string>("");
+    const navigate = useNavigate();
 
     const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
         setText(e.target.value);
@@ -16,7 +18,9 @@ const RegistrationTemplate = () => {
         if (text.trim() === "") {
             showToast({ type: EToastType.ERROR, message: "빈칸은 입력할 수 없어요.😅" });
         } else {
+            localStorage.setItem("text", text);
             showToast({ type: EToastType.SUCCESS, message: "응원해주셔서 감사해요!🥳" });
+            navigate("/");
         }
     };
 
