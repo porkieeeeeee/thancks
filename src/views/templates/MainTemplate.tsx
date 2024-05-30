a8d3d345365e11784322f5f1e2d7f6cba027e907
porkieeeeeee <porkieeeeeee@gmail.com>
2024-05-30 14:37:17 +0900

    feat::button 링크 연결

--- src/views/templates/MainTemplate.tsx
+++ src/views/templates/MainTemplate.tsx
@@ -1,13 +1,20 @@
 import HyperlinkButton from "components/atom/HyperlinkButton";
 import MainTitle from "components/atom/MainTitle";
 import MessageCardBox from "components/molecule/MessageCardBox";
+import { useNavigate } from "react-router-dom";
 
 const MainTemplate = () => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = () => {
+        navigate("/registration");
+    };
+
     return (
         <>
             <MainTitle title='감사합니다' content='응원해주셔서' />
             <MessageCardBox />
-            <HyperlinkButton label='응원하기' />
+            <HyperlinkButton label='응원하기' onClick={handleButtonClick} />
         </>
     );
 };
