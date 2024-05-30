0b686358518da666cb34f8156e9bd38206b35d9d
porkieeeeeee <porkieeeeeee@gmail.com>
2024-05-30 15:35:24 +0900

    feat::입력 값 관련 기능 구현

--- src/components/atom/TextArea.tsx
+++ src/components/atom/TextArea.tsx
@@ -1,8 +1,36 @@
+import React, { useState } from "react";
+import { EToastType, showToast } from "./Toast";
+
 interface ITextareaProps {
     placeholder?: string;
+    limit?: number;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
 }
 
-const TextArea = ({ placeholder }: ITextareaProps) => {
-    return <textarea className='w-full h-[330px] p-5 rounded-md outline-none bg-gray000' placeholder={placeholder} />;
+const TextArea = ({ placeholder, limit = 150, value, onChange }: ITextareaProps) => {
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const textvalue = e.target.value;
+        if (textvalue.length <= limit) {
+            onChange(e);
+        } else {
+            showToast({ type: EToastType.ERROR, message: `${limit}자 이하로 입력해주세요.` });
+        }
+    };
+
+    return (
+        <>
+            <textarea
+                className='w-full h-[330px] p-5 rounded-md outline-none resize-none bg-gray000'
+                placeholder={placeholder}
+                value={value}
+                onChange={handleChange}
+            />
+            <p className='absolute bottom-1 right-1 mt-2 text-sm text-gray100'>
+                {value.length} / {limit}
+            </p>
+        </>
+    );
 };
+
 export default TextArea;
--- src/views/templates/RegistrationTemplate.tsx
+++ src/views/templates/RegistrationTemplate.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import HyperlinkButton from "components/atom/HyperlinkButton";
 import MainTitle from "components/atom/MainTitle";
 import TextArea from "components/atom/TextArea";
+import { showToast, EToastType } from "components/atom/Toast"; // showToast와 EToastType을 가져옵니다.
 import SingleCenterItemBox from "components/molecule/SingleCenterItemBox";
 
 const RegistrationTemplate = () => {
+    const [text, setText] = useState<string>("");
+
+    const handleTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setText(e.target.value);
+    };
+
+    const handleSubmit = () => {
+        if (text.trim() === "") {
+            showToast({ type: EToastType.ERROR, message: "빈칸은 입력할 수 없어요.😅" });
+        } else {
+            showToast({ type: EToastType.SUCCESS, message: "응원해주셔서 감사해요!🥳" });
+        }
+    };
+
     return (
         <>
             <MainTitle content='응원 한마디 부탁드립니다.^^' />
             <SingleCenterItemBox>
-                <TextArea placeholder='ex. 포키 화이팅' />
+                <TextArea placeholder='ex. 포키 화이팅' value={text} onChange={handleTextChange} />
             </SingleCenterItemBox>
-            <HyperlinkButton label='응원 남기기' />
+            <HyperlinkButton label='응원 남기기' onClick={handleSubmit} />
         </>
     );
 };
