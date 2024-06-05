d64a9143100e83dbdbc5a11aa69d7ccec9966598
porkieeeeeee <porkieeeeeee@gmail.com>
2024-06-05 14:15:39 +0900

    [FEAT] 메시지 0건일 때 UI 추가

--- src/components/molecule/MessageCardBox.tsx
+++ src/components/molecule/MessageCardBox.tsx
@@ -1,5 +1,5 @@
-import MessageCard from "components/atom/MessageCard";
 import { useEffect, useRef, useState } from "react";
+import MessageCard from "components/atom/MessageCard";
 
 const MessageCardBox = () => {
     const [hasScroll, setHasScroll] = useState<boolean>(false);
@@ -42,9 +42,15 @@ const MessageCardBox = () => {
             ref={containerRef}
             className='absolute top-40 h-[400px] overflow-auto w-full flex flex-col gap-y-2.5 cs-scrollbar'
         >
-            {messages.map((message, index) => (
-                <MessageCard key={index} hasScroll={hasScroll} text={message} />
-            ))}
+            {messages.length > 0 ? (
+                messages.map((message, index) => <MessageCard key={index} hasScroll={hasScroll} text={message} />)
+            ) : (
+                <div className='text-center text-gray-500'>
+                    응원 메시지가 없습니다.
+                    <br />
+                    가장 먼저 포키를 응원해보세요!
+                </div>
+            )}
         </div>
     );
 };
